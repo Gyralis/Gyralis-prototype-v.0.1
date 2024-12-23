0294f25987d7937396516b5abfecaee6beb561c1
rperez89 <rodrigopi1989@gmail.com>
2024-12-23 13:36:12 -0300

    Basic diamond stuffs

---
+++ packages/foundry/contracts/facets/cut/DiamondCutFacet.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT License
+pragma solidity >=0.8.20;
+
+import { IDiamondCut, IDiamond } from "./IDiamondCut.sol";
+import { Facet } from "../Facet.sol";
+import { DiamondCutBase } from "./DiamondCutBase.sol";
+
+contract DiamondCutFacet is IDiamondCut, DiamondCutBase, Facet {
+    function DiamondCut_init() external onlyInitializing {
+        _addInterface(type(IDiamondCut).interfaceId);
+    }
+
+    /// @inheritdoc IDiamondCut
+    function diamondCut(
+        IDiamond.FacetCut[] memory facetCuts,
+        address init,
+        bytes memory initData
+    )
+        external
+        onlyDiamondOwner
+        reinitializer(_getInitializedVersion() + 1)
+    {
+        _diamondCut(facetCuts, init, initData);
+    }
+}
