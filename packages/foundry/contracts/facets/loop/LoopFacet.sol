888a4a74dcef96b24cb117e195a5a555a5bb528e
rperez89 <rodrigopi1989@gmail.com>
2025-02-08 11:50:39 -0300

    some comments

--- packages/foundry/contracts/facets/loop/LoopFacet.sol
+++ packages/foundry/contracts/facets/loop/LoopFacet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./LoopStorage.sol";
 import { AccessControlBase } from "../access-control/AccessControlBase.sol";
 import { IAccessControl } from "../access-control/IAccessControl.sol";
 import { DEFAULT_ADMIN_ROLE } from "src/Constants.sol";
@@ -15,24 +15,7 @@ contract LoopFacet is AccessControlBase {
     error NotAuthorized();
 
     uint256 public constant ONE_HUNDRED_PERCENT = 1e18;
-
-    struct Claimer {
-        uint256 registeredForPeriod;
-        uint256 latestClaimPeriod;
-    }
-
-    struct Period {
-        uint256 totalRegisteredUsers;
-        uint256 maxPayout;
-    }
-
-    ERC20 public token;
-    uint256 public periodLength;
-    uint256 public percentPerPeriod;
-    uint256 public firstPeriodStart;
-
-    mapping(address => Claimer) public claimers;
-    mapping(uint256 => Period) public periods;
+    bytes32 public constant LOOP_ADMIN_ROLE = keccak256("LOOP_ADMIN_ROLE");
 
     event Initialize(address token, uint256 periodLength, uint256 percentPerPeriod);
     event SetPercentPerPeriod(uint256 percentPerPeriod);
@@ -47,17 +30,24 @@ contract LoopFacet is AccessControlBase {
     * @param _periodLength Length of each distribution period
     * @param _percentPerPeriod Percent of total balance distributed each period
     */
-    function Loop_init(ERC20 _token, address _loopAdmin, uint256 _periodLength, uint256 _percentPerPeriod) external {
+    function Loop_init(
+        ERC20 _token,
+        address _loopAdmin,
+        uint256 _periodLength,
+        uint256 _percentPerPeriod
+    ) external {
         if (_periodLength == 0) revert InvalidPeriodLength();
         if (_percentPerPeriod > ONE_HUNDRED_PERCENT) revert InvalidPeriodPercentage();
         if (_loopAdmin == address(0)) revert NotAuthorized();
 
-        token = _token;
-        periodLength = _periodLength;
-        percentPerPeriod = _percentPerPeriod;
-        firstPeriodStart = block.timestamp;
+        LoopStorage.Layout storage ds = LoopStorage.layout();
+        ds.token = _token;
+        ds.loopAdmin = _loopAdmin;
+        ds.periodLength = _periodLength;
+        ds.percentPerPeriod = _percentPerPeriod;
+        ds.firstPeriodStart = block.timestamp;
 
-        // Assign LOOP_ADMIN_ROLE to the organization
+        // Assign LOOP_ADMIN_ROLE to the loop admin
         _setUserRole(_loopAdmin, DEFAULT_ADMIN_ROLE, true);
 
         emit Initialize(address(_token), _periodLength, _percentPerPeriod);
@@ -70,7 +60,7 @@ contract LoopFacet is AccessControlBase {
     function setPercentPerPeriod(uint256 _percentPerPeriod) external onlyAuthorized {
         if (_percentPerPeriod > ONE_HUNDRED_PERCENT) revert InvalidPeriodPercentage();
 
-        percentPerPeriod = _percentPerPeriod;
+        LoopStorage.layout().percentPerPeriod = _percentPerPeriod;
         emit SetPercentPerPeriod(_percentPerPeriod);
     }
 
@@ -78,7 +68,8 @@ contract LoopFacet is AccessControlBase {
     * @notice Register for the next period and claim if registered for the current period.
     */
     function claimAndRegister() external {
-        Claimer storage claimer = claimers[msg.sender];
+        LoopStorage.Layout storage ds = LoopStorage.layout();
+        LoopStorage.Claimer storage claimer = ds.claimers[msg.sender];
 
         uint256 currentPeriod = getCurrentPeriod();
         if (claimer.registeredForPeriod > currentPeriod) revert AlreadyRegistered();
@@ -89,7 +80,7 @@ contract LoopFacet is AccessControlBase {
 
         uint256 nextPeriod = currentPeriod + 1;
         claimer.registeredForPeriod = nextPeriod;
-        periods[nextPeriod].totalRegisteredUsers++;
+        ds.periods[nextPeriod].totalRegisteredUsers++;
 
         emit Register(msg.sender, nextPeriod);
     }
@@ -98,7 +89,8 @@ contract LoopFacet is AccessControlBase {
     * @notice Claim from the faucet without registering for the next period.
     */
     function claim() external {
-        Claimer storage claimer = claimers[msg.sender];
+        LoopStorage.Layout storage ds = LoopStorage.layout();
+        LoopStorage.Claimer storage claimer = ds.claimers[msg.sender];
         uint256 currentPeriod = getCurrentPeriod();
 
         if (!_canClaim(claimer, currentPeriod)) revert CannotClaim();
@@ -111,8 +103,9 @@ contract LoopFacet is AccessControlBase {
     * @param _to Address to withdraw to
     */
     function withdrawDeposit(address _to) external onlyAuthorized {
-        uint256 balance = token.balanceOf(address(this));
-        token.transfer(_to, balance);
+        LoopStorage.Layout storage ds = LoopStorage.layout();
+        uint256 balance = ds.token.balanceOf(address(this));
+        ds.token.transfer(_to, balance);
         emit Withdraw(msg.sender, _to, balance);
     }
 
@@ -120,7 +113,7 @@ contract LoopFacet is AccessControlBase {
     * @notice Get the current period number.
     */
     function getCurrentPeriod() public view returns (uint256) {
-        return (block.timestamp - firstPeriodStart) / periodLength;
+        return (block.timestamp - LoopStorage.layout().firstPeriodStart) / LoopStorage.layout().periodLength;
     }
 
     /**
@@ -128,20 +121,22 @@ contract LoopFacet is AccessControlBase {
     * @param _periodNumber Period number
     */
     function getPeriodIndividualPayout(uint256 _periodNumber) public view returns (uint256) {
-        Period storage period = periods[_periodNumber];
+        LoopStorage.Layout storage ds = LoopStorage.layout();
+        LoopStorage.Period storage period = ds.periods[_periodNumber];
         return _getPeriodIndividualPayout(period);
     }
 
-    function _canClaim(Claimer storage claimer, uint256 currentPeriod) internal view returns (bool) {
+    function _canClaim(LoopStorage.Claimer storage claimer, uint256 currentPeriod) internal view returns (bool) {
         bool userRegisteredCurrentPeriod = claimer.registeredForPeriod == currentPeriod;
         bool userYetToClaimCurrentPeriod = claimer.latestClaimPeriod < currentPeriod;
 
         return userRegisteredCurrentPeriod && userYetToClaimCurrentPeriod;
     }
 
-    function _claim(Claimer storage _claimer, uint256 _currentPeriod) internal {
-        Period storage period = periods[_currentPeriod];
-        uint256 faucetBalance = token.balanceOf(address(this));
+    function _claim(LoopStorage.Claimer storage _claimer, uint256 _currentPeriod) internal {
+        LoopStorage.Layout storage ds = LoopStorage.layout();
+        LoopStorage.Period storage period = ds.periods[_currentPeriod];
+        uint256 faucetBalance = ds.token.balanceOf(address(this));
 
         if (faucetBalance == 0) revert FaucetBalanceIsZero();
 
@@ -151,21 +146,22 @@ contract LoopFacet is AccessControlBase {
         }
 
         uint256 claimerPayout = _getPeriodIndividualPayout(period);
-        token.transfer(msg.sender, claimerPayout);
+        ds.token.transfer(msg.sender, claimerPayout);
 
         _claimer.latestClaimPeriod = _currentPeriod;
         emit Claim(msg.sender, _currentPeriod, claimerPayout);
     }
 
     function _getPeriodMaxPayout(uint256 _faucetBalance) internal view returns (uint256) {
-        return (_faucetBalance * percentPerPeriod) / ONE_HUNDRED_PERCENT;
+        LoopStorage.Layout storage ds = LoopStorage.layout();
+        return (_faucetBalance * ds.percentPerPeriod) / ONE_HUNDRED_PERCENT;
     }
 
-    function _getPeriodIndividualPayout(Period storage period) internal view returns (uint256) {
+    function _getPeriodIndividualPayout(LoopStorage.Period storage period) internal view returns (uint256) {
         if (period.totalRegisteredUsers == 0) return 0;
 
         uint256 periodMaxPayout = period.maxPayout == 0
-            ? _getPeriodMaxPayout(token.balanceOf(address(this)))
+            ? _getPeriodMaxPayout(LoopStorage.layout().token.balanceOf(address(this)))
             : period.maxPayout;
 
         return periodMaxPayout / period.totalRegisteredUsers;
