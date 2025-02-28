efe55d65cfc4f3c6f8aca993589a9a1d3cb1fd3a
Matias <mantitaaa89@gmail.com>
2025-02-28 16:42:03 -0300

    Refactor:OrganizationCards and update layout + small tweaks

--- packages/nextjs/app/prototype/page.tsx
+++ packages/nextjs/app/prototype/page.tsx
@@ -18,7 +18,7 @@ const Prototype: NextPage = () => {
       </header>
       <main>
         <div className="mx-auto max-w-7xl py-4">
-          <Example />
+          <OrganizationCards />
         </div>
       </main>
     </div>
@@ -27,38 +27,36 @@ const Prototype: NextPage = () => {
 
 export default Prototype;
 
-const people = [
+const organizations = [
   {
     name: "1hive",
     description: "1Hive is a DAO that issues and distributes a digital currency called Honey.",
-    role: "Admin",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    role: "Admin"
   },
 ];
 
-const Example = () => {
+const OrganizationCards = () => {
   return (
-    <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {people.map(person => (
+    <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {organizations.map(org => (
         <li
-          key={person.name}
-          className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow shadow-md cursor-pointer hover:shadow-lg transition-all ease-in-out duration-300"
+          key={org.name}
+          className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow-md cursor-pointer hover:shadow-lg transition-all ease-in-out duration-300"
         >
           <div className="flex flex-1 flex-col p-8">
             {/* <img alt="" src={"/1hive-logo.png"} className="mx-auto shrink-0 rounded-full" /> */}
             <Image alt="" src={"/1hive-logo.svg"} className="mx-auto shrink-0 rounded-full" width={100} height={100} />
-            <h2 className="mt-6 font-bold text-gray-900 text-center">{person.name}</h2>
+            <h2 className="mt-6 font-bold text-gray-900 text-center">{org.name}</h2>
             <dl className="mt-1 flex grow flex-col justify-between">
               <dt className="sr-only">description</dt>
-              <dd className="text-sm text-gray-500">{person.description}</dd>
+              <dd className="text-sm text-gray-500">{org.description}</dd>
             </dl>
           </div>
           <div>
             <div className="-mt-px flex divide-x divide-gray-200">
               <div className="flex w-0 flex-1">
                 <a
-                  href={`/prototype/${person.name}`}
+                  href={`/prototype/${org.name}`}
                   className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                 >
                   Active Loops
