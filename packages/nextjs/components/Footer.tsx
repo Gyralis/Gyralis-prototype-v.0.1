6c6cd161c9ceab89d081670359c7690d321fbda2
Matias <mantitaaa89@gmail.com>
2025-05-26 15:11:00 -0300

    fix X link

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -21,7 +21,7 @@ export const Footer = () => {
             <p className="text-sm">Gyralis 2025 - All rights reserved.</p>
           </div>
           <div className="">
-            <a href="https://x.com/gyralis82742" target="_blank" className="flex items-center space-x-2">
+            <a href="https://x.com/gyralis_xyz" target="_blank" className="flex items-center space-x-2">
               <span className="text-sm">Follow us</span>
               <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 50 50">
                 <path
